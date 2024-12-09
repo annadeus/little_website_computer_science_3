f1c5c04b5287ef93e4b3a0dbe3e1e8e9d21bb8a2
flashwing-04 <charlotte.john.04@gmx.de>
2024-12-09 16:34:15 +0100

    add more template stubs

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from profiles import views
 
 urlpatterns = [
-path("profiles/<int:pk>/", views.ProfileDetailView.as_view(), name="profile_detail"),
+path("profiles/<int:pk>/", views.ProfileDetailView.as_view(), name="profile_index"),
     path("profiles/<int:pk>/update/", views.ProfileUpdateView.as_view(), name="profile_update"),
     path("profiles/<int:pk>/delete/", views.ProfileDeleteView.as_view(), name="profile_delete"),
 ]
